61b9593b7d7280be0638b60dfd61c515acfb6a59
e1n4ach <ilya-sitnikov-2020@list.ru>
2024-06-22 20:56:26 +0300

    psichologyPhrase

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,6 +13,43 @@ export default function HomePage() {
       <MainSection>
         <IntroSection>ПСИХОЛОГ</IntroSection>
         <AssistantSection />
+        <div className='mainPhrasePsicholog'>
+          <div className='PhrasePsicholog1'>
+            <a classname="textPsicholog">
+              Понять причины стрессовых состояний, справиться с ними
+            </a>
+          </div>
+          <div className='PhrasePsicholog1'>
+            <a classname="textPsicholog">
+              Сформировать реалистичную, позитивную и устойчивую самооценку
+            </a>
+          </div>
+          <div className='PhrasePsicholog1'>
+            <a classname="textPsicholog">
+              Открыть свой потенциал и реализовать жизненные планы
+            </a>
+          </div>
+          <div className='PhrasePsicholog1'>
+            <a classname="textPsicholog">
+              Преодолеть трудности и кризисы, адаптироваться к изменениям
+            </a>
+          </div>
+          <div className='PhrasePsicholog1'>
+            <a classname="textPsicholog">
+              Находить выходы из конфликтов, решать проблемы и достигать цели
+            </a>
+          </div>
+          <div className='PhrasePsicholog1'>
+            <a classname="textPsicholog">
+              Налаживать взаимоотношения с окружающими
+            </a>
+          </div>
+          <div className='PhrasePsicholog1'>
+            <a classname="textPsicholog">
+              Научиться эффективно общаться и выражать свои эмоции
+            </a>
+          </div>
+        </div>
       </MainSection>
       <Footer />
     </>
